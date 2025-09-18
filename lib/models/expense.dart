bfd5d4a92028f30a61ef2ad3208f78ed1601813a
Espérant KELA <esperant.kela@gmail.com>
2025-09-18 12:40:51 +0100

    feat(chart): implement Chart and ChartBar widgets for visualizing expenses

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,38 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 
-enum Category {
-  food,
-  leisure,
-  travel,
-  work,
-  others,
-}
+enum Category { food, leisure, travel, work, others }
 
 const categoryIcons = {
   Category.food: Icons.lunch_dining,
   Category.leisure: Icons.movie,
-  Category.travel:Icons.flight_takeoff,
+  Category.travel: Icons.flight_takeoff,
   Category.work: Icons.work,
 };
 
-final  formatter = DateFormat.yMd();
+final formatter = DateFormat.yMd();
 const uuid = Uuid();
+
 class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.date,
     required this.category,
-  }) : id =  Uuid().v4();
+  }) : id = Uuid().v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
-  final Category category ;
+  final Category category;
 
-  String get getFormattedDate{
+  String get getFormattedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+    : expenses = allExpenses
+          .where((expense) => expense.category == category)
+          .toList();
+
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
   }
 }
